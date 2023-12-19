55e63a5224dc8250e5cbd98bfa30e68adb06cf50
Denis Carriere <carriere.denis@gmail.com>
2023-12-18 19:22:25 -0500

    add deploy address

--- sql/groupBySupply.sql
+++ sql/groupBySupply.sql
@@ -1,6 +1,7 @@
 SELECT
   tick,
   first_value(p) as protocol,
+  first_value(deploy.from_address) as deploy_address,
   first_value(timestamp) as deploy_timestamp,
   last_value(block_number) as last_block_number,
   last_value(timestamp) as last_timestamp,
