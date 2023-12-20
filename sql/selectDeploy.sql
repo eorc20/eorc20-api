1c9500a2495bb868bf521a7fb17fd8eea4964142
Denis Carriere <carriere.denis@gmail.com>
2023-12-19 23:26:28 -0500

    update deploy

--- sql/selectDeploy.sql
+++ sql/selectDeploy.sql
@@ -5,5 +5,6 @@ SELECT
     block_number as deploy_block_number,
     max as max_supply,
     lim as limit_by_amount,
-    p as protocol
+    p as protocol,
+    length(toString(limit_by_amount)) - 1 as decimal
 FROM deploy ORDER BY block_number
