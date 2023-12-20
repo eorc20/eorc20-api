5ab1b25e8cef6c2758a9b551a70a9a97253e9e7e
Denis Carriere <carriere.denis@gmail.com>
2023-12-19 23:55:07 -0500

    group by

--- sql/groupTokensByAddress.sql
+++ sql/groupTokensByAddress.sql
@@ -1,12 +1,8 @@
 SELECT
+    tick,
     last_value(from) as address,
     sum(toUInt64(mint.amt)) as amount,
-    last_value(mint.timestamp) as last_updated,
-    tick,
-    first_value(deploy.timestamp) as tick_created,
-    first_value(deploy.id) as tick_id
+    last_value(mint.timestamp) as last_updated
 FROM mint
-INNER JOIN deploy
-ON deploy.tick = mint.tick AND deploy.lim = mint.amt AND mint.from = '0x413d069295a50db188d8eaa29973e2866bbcbc03'
-GROUP BY (from, tick)
-ORDER BY amount DESC;
+WHERE tick = 'eoss' AND amt = 10000 AND block_number <= 21563992 AND from = {address: String}
+GROUP BY tick;
